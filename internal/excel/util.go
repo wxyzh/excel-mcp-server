e89bd279a24f5bc867998bb28fd4a01378f8a0ce
Kazuki Negoro <negokaz@gmail.com>
2025-07-19 21:21:13 +0900

    Support single-cell notation

--- internal/excel/util.go
+++ internal/excel/util.go
@@ -9,9 +9,9 @@ import (
 	"github.com/xuri/excelize/v2"
 )
 
-// parseRange parses Excel's range string (e.g. A1:C10)
+// parseRange parses Excel's range string (e.g. A1:C10 or A1)
 func ParseRange(rangeStr string) (int, int, int, int, error) {
-	re := regexp.MustCompile(`(\$?[A-Z]+\$?\d+):(\$?[A-Z]+\$?\d+)`)
+	re := regexp.MustCompile(`^(\$?[A-Z]+\$?\d+)(?::(\$?[A-Z]+\$?\d+))?$`)
 	matches := re.FindStringSubmatch(rangeStr)
 	if matches == nil {
 		return 0, 0, 0, 0, fmt.Errorf("invalid range format: %s", rangeStr)
@@ -20,6 +20,12 @@ func ParseRange(rangeStr string) (int, int, int, int, error) {
 	if err != nil {
 		return 0, 0, 0, 0, err
 	}
+
+	if matches[2] == "" {
+		// Single cell case
+		return startCol, startRow, startCol, startRow, nil
+	}
+
 	endCol, endRow, err := excelize.CellNameToCoordinates(matches[2])
 	if err != nil {
 		return 0, 0, 0, 0, err
